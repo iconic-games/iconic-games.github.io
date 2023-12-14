5804ba2d0245ff5ca8444729783412c150303216
John Lusty <johnlusty@gmail.com>
2023-12-14 19:32:10 +0000

    careers improvements

--- js/career-scroll.js
+++ js/career-scroll.js
@@ -1,6 +1,19 @@
 window.addEventListener("wheel", function (e) {
 	var jobScroller = document.getElementById("jobs-list");
-	if (e.deltaY > 0) jobScroller.scrollLeft += 100;
-	else jobScroller.scrollLeft -= 100;
+	var cultureScroller = document.getElementById("culture-list");
+	
+	var scroller = document.getElementById("jobs").hidden ? cultureScroller : jobScroller;
+	
+	if (e.deltaY > 0) scroller.scrollLeft += 100;
+	else scroller.scrollLeft -= 100;
 	console.log("Scroll " + e.deltaY);
 });
+
+function showCulture() {
+	document.getElementById("jobs").hidden = true;
+	document.getElementById("culture").hidden = false;
+}
+function showJobs() {
+	document.getElementById("culture").hidden = true;
+	document.getElementById("jobs").hidden = false;
+}
