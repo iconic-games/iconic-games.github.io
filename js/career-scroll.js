b13c13f110237a32329a9f0f20e65cbf9f176467
John Lusty <johnlusty@gmail.com>
2023-12-17 10:08:43 +0000

    Further work for portrait / mobile (WIP)

--- js/career-scroll.js
+++ js/career-scroll.js
@@ -4,16 +4,17 @@ window.addEventListener("wheel", function (e) {
 	var jobScroller = document.getElementById("jobs-list");
 	var cultureScroller = document.getElementById("culture-list");
 	
-	var scroller = document.getElementById("jobs").hidden ? cultureScroller : jobScroller;
-	
-	if(scroller != null) 
-	{
-		if(e.deltaX != 0) {
-			touch = true;
-		} else if(touch == false)
+	var scroller = document.getElementById("jobs")?.hidden ? cultureScroller : jobScroller ?? jobScroller;
+	if(scroller.clientHeight < scroller.clientWidth) { // Wheel for horizontal scrollers
+		if(scroller != null) 
 		{
-			if (e.deltaY > 0) scroller.scrollLeft += 100;
-			else scroller.scrollLeft -= 100;			
+			if(e.deltaX != 0) {
+				touch = true;
+			} else if(touch == false)
+			{
+				if (e.deltaY > 0) scroller.scrollLeft += 100;
+				else scroller.scrollLeft -= 100;			
+			}
 		}
 	}
 });
